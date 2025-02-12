75fc572bb8c9fc66fccbdd78bccb18c6afb6cb78
cathytham <103431943+cathytham@users.noreply.github.com>
2025-02-12 01:15:10 -0500

    Applied principles of Jungian psychology

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ load_dotenv()  # Load environment variables from .env
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")  # Securely load API key
 
-# Initialize the OpenAI client with the API key from environment variables
-#client = OpenAI(os.getenv("OPENAI_API_KEY"))
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
@@ -25,25 +22,25 @@ def index():
         try:
             response = openai.chat.completions.create(
                 model="gpt-4o-mini",  
-                messages=[{"role": "developer", "content": "You are a psychedelic AI that speaks in Oulipian constraints. Your responses are short, surreal, and witty. Use mathematical games, lipograms, palindromes, or poetic structures to shape your language. Avoid predictable phrasing. Let logic slip through the cracks like liquid geometry."}, 
+                messages=[{"role": "developer", "content": "You are a Jungian psychoanalyst, an expert in dream interpretation, using Carl Jung's psychological theories that uses concept like archetypes, the collective unconscious, and the process of individuation. Analyse the dreamer's dream and focus on symbolic elements such as figures, actions, and settings. Your responses are detailed and comprehensive(200 words) that is easy to understand and relate to the dreamer's dream."}, 
                           {"role": "user", "content": prompt}],
                           temperature=1.2,
-                          max_completion_tokens=50
+                          max_completion_tokens=250
             )
             result = response.choices[0].message.content
-
             #generate the image url from the DALL-E API
             response = openai.images.generate(
-                prompt=prompt,
+                model="dall-e-3",
+                prompt="In a dreamlike style, create a visual representations of the interpreted dreams.:"+prompt,
                 n=1,
-                size="1024x1024"
+                size="1024x1024",
+                quality="standard"
             )
             image_url = response.data[0].url
-            print(image_url)
             
         except Exception as e:
             result = f"Error: {str(e)}"
-    return render_template("index.html", result=result)
+    return render_template("index.html", result=result, image_url=image_url)
 
 if __name__ == "__main__":
     app.run(debug=True)  # Run locally for testing
